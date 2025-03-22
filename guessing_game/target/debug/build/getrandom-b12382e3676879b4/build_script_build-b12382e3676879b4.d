2a4f227f8e01ac41ad56415092e992fe00a8c75b
Sudip Paudel <sudippaudel512@gmail.com>
2025-03-22 20:17:12 +0545

    feat: rustbook guessgame

--- guessing_game/target/debug/build/getrandom-b12382e3676879b4/build_script_build-b12382e3676879b4.d
+++
@@ -1,5 +0,0 @@
-/home/kage/projects/rust/learnrust/guessing_game/target/debug/build/getrandom-b12382e3676879b4/build_script_build-b12382e3676879b4: /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.2/build.rs
-
-/home/kage/projects/rust/learnrust/guessing_game/target/debug/build/getrandom-b12382e3676879b4/build_script_build-b12382e3676879b4.d: /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.2/build.rs
-
-/home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.2/build.rs:
--- guessing_game/target/debug/deps/ppv_lite86-aa462fe2406d46dc.d
+++
@@ -1,9 +0,0 @@
-/home/kage/projects/rust/learnrust/guessing_game/target/debug/deps/libppv_lite86-aa462fe2406d46dc.rmeta: /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs
-
-/home/kage/projects/rust/learnrust/guessing_game/target/debug/deps/ppv_lite86-aa462fe2406d46dc.d: /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs /home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs
-
-/home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs:
-/home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs:
-/home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs:
-/home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs:
-/home/kage/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs:
