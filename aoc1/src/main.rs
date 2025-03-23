9fa773a4108965be278f40f0c8edf450809be49d
pdlsudip <sudippaudel512@gmail.com>
2025-03-23 20:33:07 +0545

    rust structs

---
+++ aoc1/src/main.rs
@@ -0,0 +1,22 @@
+use std::fs;
+
+
+fn main() {
+    let mut veca: Vec<i64> = Vec::new();
+    let mut vecb: Vec<i64> = Vec::new();
+    let inputs = fs::read_to_string("../../a.txt").expect("Failed to read file");
+
+    for line in inputs.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        veca.push(parts[0].parse().expect("Invalid number"));
+        vecb.push(parts[1].parse().expect("Invalid number"));
+    }
+
+    veca.sort();
+    vecb.sort();
+    let mut sum: i64 = 0;
+    for i in 0..veca.len().min(vecb.len()) {
+        sum += (veca[i] - vecb[i]).abs();
+    }
+    println!("{}", sum);
+}
---
+++ aoc1/target/debug/aoc1.d
@@ -0,0 +1,1 @@
+/home/kage/projects/rust/learnrust/aoc1/target/debug/aoc1: /home/kage/projects/rust/learnrust/aoc1/src/main.rs
